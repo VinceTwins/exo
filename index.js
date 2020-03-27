adaed83a4b1bdc20af7947e0f2fe53bcd120c5db
VinceTwins <58826083+VinceTwins@users.noreply.github.com>
2020-03-27 10:10:54 +0100

    file settings.json created and added in .gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require(express);
+const mysql = require("mysql");
+const settings = require("./settings.json");
+
+const sqlConfig = settings.sqlConfig;
+    
+const app = express();
+app.listen(3000, () =>  {
+    console.log("SERVER STARTED !");
+});
