eb6c24d441b47e5dbb87c8338de348b5e58a6e69
VinceTwins <58826083+VinceTwins@users.noreply.github.com>
2020-03-27 12:40:56 +0100

    /api/articles + /api/articles/create path added

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ app.listen(3000, () =>  {
 app.use(express.static("./public"));
 app.use(express.urlencoded({extended: true}));
 
+app.get("/api/articles", (req, res) => {
+    const sqlConnection = mysql.createConnection(sqlConfig);
+    sqlConnection.query(
+        "SELECT id, title, content, author, created_at FROM node_articles WHERE id = 1 LIMIT 1",
+        (error, result) => {
+            if (error) {
+                console.log("ERROR :", error.code);
+            } else {
+                res.send(result[0]);
+            }
+            sqlConnection.end();
+        }
+    );
+});
+
+app.route("/api/articles/create")
+    .get((req, res) => res.status(503).send({status: "error"}))
+    .post((req, res) => {
+        console.log(req.body);
+        const sqlConnection = mysql.createConnection(sqlConfig);
+        sqlConnection.query(
+            "INSERT INTO node_articles VALUES (NULL, ?, ?, ?, ?)",
+            [req.body.title, req.body.content, req.body.author, req.body.creationDate],
+            (error, result) => {
+                if (error) {
+                    console.log("error :", error.code);
+                    res.status(503).send({status: "error"});
+                } else {
+                    console.log(result);
+                    res.send({status: "OK"});
+                }
+                sqlConnection.end();
+            });    
+    });
